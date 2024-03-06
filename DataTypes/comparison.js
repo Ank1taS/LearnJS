37d94d13d0d290e80de4c0fa273bd87ad1f8f217
Ankita Sahoo <sahoosona2209@gmail.com>
2024-03-06 11:17:04 +0530

    add results with eplaination

--- DataTypes/comparison.js
+++ DataTypes/comparison.js
@@ -6,10 +6,10 @@ console.log("0.0006" > 1 );
 //Give Away ::  it is better ghabit to compare same datatype as type conversion may not give predictable values
 
 
-console.log(null > 0);      // false
-console.log(null >= 0);     // true
-console.log(null == 0);     // false
-console.log(null <= 0);     // true
+console.log(null > 0);      // false    :  JavaScript converts null to a number, which becomes 0. Therefore, the comparison becomes 0 > 0, which is false.
+console.log(null >= 0);     // true     :  null is converted to a number, which is 0. So, the comparison becomes 0 >= 0
+console.log(null == 0);     // false    : the equality operator attempts to perform type coercion[Type coercion is the automatic or implicit conversion of values from one data type to another] if the operands have different types. Since null is considered to be loosely equal (==) to undefined 
+console.log(null <= 0);     // true     : null is converted to a number, which is 0. So, the comparison becomes 0 <= 0
 
 
 console.log(undefined > 0);     // false
